ee2c0472894daf96bedbae84e0fc54fee7ddc24d
TicketsData <info@ticketsdata.com>
2025-09-26 14:31:31 +0300

    Simple curl_example.sh

---
+++ curl_example.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -X GET "https://api.ticketsdata.com/v1/event?url=https://www.ticketmaster.com/sabrina-carpenter-short-n-sweet-tour-pittsburgh-pennsylvania-10-23-2025/event/160062583FD9756B" \
+  -H "Authorization: Bearer YOUR_API_KEY"
